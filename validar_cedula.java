9bd8dac5b97015cf13c7fc9086335e7979c823b6
Ramon Lopez <evercuevas1000@gmail.com>
2018-09-21 11:39:52 -0400

    Update validar_cedula.java

--- validar_cedula.java
+++ validar_cedula.java
@@ -1,86 +1,64 @@
 /**
-* Comprueba el correcto formato de las cédula dominicana pasada
-* por parámetro.
-*
-*
-@param cedula
-*        
-Documento equivalente al DNI español.
-*
-*
-@return verdadero si tiene el formato correcto
-*
-Ejemplo para prueba: 00118595792
-*/
+ * Comprueba el correcto formato de las cédula dominicana pasada
+ * por parámetro.
+ *
+ *
+ * @param cedula
+ *
+ *
+ * @return verdadero si tiene el formato correcto
+ *
+ * Ejemplo para prueba: 00118595792
+ */
 
-
-public static boolean validarCedula(String cedula)
-{
-
+public static boolean validarCedula(String cedula){
+	
     int suma = 0;
 
     final char[] peso = { '1', '2', '1', '2', '1', '2', '1', '2', '1', '2' };
 
     // Comprobaciones iniciales
-
-
-    if ((cedula == null) || (cedula.length() != 11))
-            return false;
-
-    // Si no es un nº => la descartamos
-   
-
- 	try
-    {Long.parseLong(cedula);}
-
-    		
-	catch (Exception e)
-    {
-            return false;
+	
+    if ((cedula == null) || (cedula.length() != 11)){
+    	return false;
     }
 
+    // Si no es un nº => la descartamos  
 
-
+ 	try{
+		Long.parseLong(cedula);
+	}
+	catch (Exception e){
+    	return false;
+	}
     
-	for (int i = 0; i < 10; i++)
-    {
+	for (int i = 0; i < 10; i++){
 
+		int a = Character.getNumericValue(cedula.charAt(i));
 
-		 int a = Character.getNumericValue(cedula.charAt(i));
-
-	         int b = Character.getNumericValue(peso[i]);
-
+		int b = Character.getNumericValue(peso[i]);
 	
-	         char[] mult = Integer.toString(a * b).toCharArray();
+        char[] mult = Integer.toString(a * b).toCharArray();
 
-	         if (mult.length > 1)
-{
-
+	    if (mult.length > 1){
 			a = Character.getNumericValue(mult[0]);
+            b = Character.getNumericValue(mult[1]);
+        }
+		else{
+           	a = 0;
+            b = Character.getNumericValue(mult[0]);
+    	}
 
-	                b = Character.getNumericValue(mult[1]);
-
-	         }
-else
-{
-
-                    	a = 0;
-	                b = Character.getNumericValue(mult[0]);
-
-            	 }
-
-	
 		suma = suma + a + b;
 	}
 
-	
 	int digito = (10 - (suma % 10)) % 10;
 
     // Comprobamos que el dígito de control coincide
     
-
-	if (digito != Character.getNumericValue(cedula.charAt(10)))
-            return false;
+	if (digito != Character.getNumericValue(cedula.charAt(10))){
+	    return false;
+	}
 
     return true;
 }
